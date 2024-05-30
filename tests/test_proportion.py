d5aca6a7cb2f94a4541b29e6c6320fde027d43d3
Vladyslav <fareomg@gmail.com>
2024-05-30 19:46:38 +0300

    feat(dri-1032): add the ability to support measure (unit group)

--- tests/test_proportion.py
+++ tests/test_proportion.py
@@ -2,8 +2,10 @@ from .utils import convert_units
 
 # test cases
 cases = [
-    {"from": "%", "amount": 1, "to": "Fraction", "expected": 0.01},
-    {"from": "Fraction", "amount": 1, "to": '%', "expected": 100},
+    {"from": "%", "amount": 1, "to": "Fraction", "expected": 0.01,
+     "measure": 'proportion'},
+    {"from": "Fraction", "amount": 1, "to": '%', "expected": 100,
+     "measure": 'proportion'},
 ]
 
 
