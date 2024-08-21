314cbd610fb8127d735e749936db08aa9441f2dd
Vladyslav <fareomg@gmail.com>
2024-08-21 20:07:03 +0300

    feat(dri-1308): add unitless unit

--- src/corva_unit_converter/converter.py
+++ src/corva_unit_converter/converter.py
@@ -35,7 +35,8 @@ measures = {
     "volume_concentration": definitions.volume_concentration.rule,
     "volume_flow_rate": definitions.volume_flow_rate.rule,
     "voltage": definitions.voltage.rule,
-    "viscosity": definitions.viscosity.rule
+    "viscosity": definitions.viscosity.rule,
+    "unitless": definitions.unitless.rule,
 }
 
 
---
+++ src/corva_unit_converter/definitions/unitless.py
@@ -0,0 +1,30 @@
+metric = imperial = {
+   "unitless": {
+      "name": {
+         "singular": "unitless",
+         "plural": "unitless",
+         "display": "unitless"
+      },
+      "to_anchor": 1,
+      "aliases": [
+        "On/Off",
+        "None",
+        "EUC"
+      ]
+   },
+}
+
+rule = {
+   "metric": metric,
+   "imperial": imperial,
+   "_anchors": {
+      "metric": {
+         "unit": "unitless",
+         "ratio": 1
+      },
+      "imperial": {
+         "unit": "unitless",
+         "ratio": 1
+      }
+   }
+}
